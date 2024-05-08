1a269172ddad7494fe1eae21a7d931924adccb8a
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-05-08 11:45:33 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,66 @@
+// 1 vazifa
+// let son1 = Number(prompt("Birinchi son kiriting: "));
+// let son2 = Number(prompt("Ikinchi son kiriting: "));
+// let summa = son1 + son2;
+
+// alert(son1 + " + " + son2 + " = " + summa);
+
+
+// 2 vaziva
+// let a = '12';
+// let b = Number(a);
+
+// alert(b);
+
+
+// 3 vaziva
+// let son = Math.floor(Math.random() * 100);
+
+// if (taxminiyson % 2 === 0) {
+//     console.log("taxminiyson juft")
+// } else {
+//     console.log(taxminiyson + "t oq")
+// }
+
+
+// 4 vaziva
+
+// let soz = prompt("Bironta soz kiriting");
+
+// if (soz === "mars") {
+//     console.log("mars");
+// } else {
+//     console.log("mars it scchool")
+// }
+
+
+// 5 vaziva
+
+// let ismlar = ['abdulox', 'aziz', 'ismayl', 'oyatulox', 'xojayv'];
+// let natija = [];
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(ismlar[i])
+//     if (ismlar[i].length > 5) {
+//         natija.push(ismlar[i]);
+//     }
+// }
+
+// console.log(natija);
+
+// 6 vaziva
+
+// let juft = [];
+// let toq = [];
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+//     if (i % 2 === 0) {
+//         juft.push(i);
+//     } else {
+//         toq.push(i);
+//     }
+// }
+
+// console.log(juft);
+// console.log(toq);
